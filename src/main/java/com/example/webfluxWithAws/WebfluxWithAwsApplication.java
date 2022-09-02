e3dd1cf4d2c0daae6dedb43d221c5acdbbb0de14
TSRI\Jeff <jeff.hsu@frog-jump.com>
2022-09-02 11:35:34 +0800

    init project

---
+++ src/main/java/com/example/webfluxWithAws/WebfluxWithAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxWithAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxWithAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxWithAwsApplication.class, args);
+	}
+
+}
