07a39704520f0c7180aa14957632c0152b43bc4d
TSRI\Jeff <jeff.hsu@frog-jump.com>
2022-09-02 14:10:49 +0800

    test guess

--- src/main/java/com/example/webfluxWithAws/controller/TestController.java
+++ src/main/java/com/example/webfluxWithAws/controller/TestController.java
@@ -1,17 +1,64 @@
 package com.example.webfluxWithAws.controller;
 
 import com.example.webfluxWithAws.domain.AWSDb;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.webfluxWithAws.repo.AwsDBRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @RestController
+@CrossOrigin("*")
 public class TestController {
 
-    @RequestMapping("/hello/{jsonData}")
-    public Mono<AWSDb> hello(@PathVariable("jsonData") String jsonData) {
+    @Autowired
+    private AwsDBRepo awsDBRepo;
 
-        return Mono.just(new AWSDb("dala", jsonData));
+
+    @RequestMapping("/hello")
+    public Mono<AWSDb> hello(@RequestBody String jsonData) {
+        AWSDb awsDb = new AWSDb();
+        awsDb.setJsonData(jsonData);
+        //awsDBRepo.save(awsDb);
+
+        return Mono.just(awsDb);
     }
+
+
+    @RequestMapping("/testMono")
+    public Mono<AWSDb> testMono(){
+        String[] ids = new String[] {"12333", "55668b40-c994-48dd-af0d-2e24820d00dd", "b46234ef-16a8-4b51-b073-b15a16f3a619"};
+
+        return Mono.just(awsDBRepo.getById(ids[0]));
+    }
+
+    @RequestMapping(value = "/testFlux")
+    public Flux<String> testFluxII(){
+        String[] ids = new String[] {"12333", "55668b40-c994-48dd-af0d-2e24820d00dd", "b46234ef-16a8-4b51-b073-b15a16f3a619"};
+
+        return Flux.fromStream(IntStream.range(0, 3).mapToObj(I -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "flux data—" + awsDBRepo.getById(ids[I]);
+        }));
+    }
+
+    @RequestMapping("testFluxSub")
+    public Flux<Stream> testFluxSub(@RequestBody String str){
+        Stream stream = IntStream.range(0,20).mapToObj(I -> {
+            return str;
+        });
+
+        return Flux.just(stream);
+    }
+
 }
